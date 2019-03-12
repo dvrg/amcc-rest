089914388c990c554353cbbfd901deb594335103
genpati <genpati@pop-os.localdomain>
2019-03-12 23:34:05 +0700

    edit url database

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ migrate = Migrate(app, db)
 Bootstrap(app)
 
 #config
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
